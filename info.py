0e5d8bc17d956271a69320e3933912f30ed19b16
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2024-02-05 18:04:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://yodipe4424:eEAusA10t3ejbvy4@cluster0.i4ht8dg.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "yodipe4424")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://stbfvbzo:eEAusA10t3ejbvy3@cluster0.hmvurbf.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
